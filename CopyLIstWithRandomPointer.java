76191bf3e36b18ec1e9e95ab515d6dd70e476c51
Yaroslav Strontsitskyi <lumstery@gmail.com>
2024-03-04 23:00:46 +0100

    Create CopyLIstWithRandomPointer.java

---
+++ CopyLIstWithRandomPointer.java
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+    if (head == null) return null;
+    
+    Map<Node, Node> map = new HashMap<Node, Node>();
+    
+    // loop 1. copy all the nodes
+    Node node = head;
+    while (node != null) {
+        map.put(node, new Node(node.val));
+        node = node.next;
+    }
+    
+    // loop 2. assign next and random pointers
+    node = head;
+    while (node != null) {
+        map.get(node).next = map.get(node.next);
+        map.get(node).random = map.get(node.random);
+        node = node.next;
+    }
+    
+    return map.get(head);
+    }
+}
+/*
+ Approach: We fill map with node from original list as key and 
+ node copy as value, then we just link copy nodes in the same way
+ how original node is linked
+
+ Time complexity: O(n)
+ Space complexity: O(n)
+*/
